af6ea3240d7dd526f01411526f6e25b5a449e0ec
Sahinur Islam <infosahinur@gmail.com>
2021-06-15 02:26:42 +0600

    database data send done

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ const static_path = path.join(__dirname, '../public');
 const template_path = path.join(__dirname, '../templates/views');
 const partials_path = path.join(__dirname, '../templates/partials');
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
 app.use(express.static(static_path));
 app.set("view engine", "hbs");
 app.set("views", template_path);
@@ -29,8 +32,26 @@ app.get('/register', (req, res) =>{
 // create a new user in database
 app.post('/register', async(req, res) =>{
     try{
-        
+        const password = req.body.password
+        const cpassword = req.body.cnpassword
 
+        if(password === cpassword){
+            const registerEmployee = new Register({
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                email: req.body.email,
+                gender: req.body.gender,
+                phone: req.body.phone,
+                age: req.body.age,
+                password: password,
+                cnpassword: cpassword
+            })
+
+            const registered = await registerEmployee.save();
+            res.status(201).render('index')
+        }else{
+            res.send("Password not matching")
+        }
     }catch(e){
         res.status(400).send(e);
     }
